5b9238736afa833b11e450bfd4afae88934e1ccc
Anthony COLAS <anthonycolas.france@gmail.com>
2020-07-13 15:23:08 +0200

    Seach movies OK

--- Components/Search.js
+++ Components/Search.js
@@ -1,32 +1,65 @@
 // Components/Search.js
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, FlatList, Text } from 'react-native'
+import { StyleSheet, View, TextInput, Button, FlatList, Text, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
 	constructor(props) {
 		super(props)
-		this._films = []
+		this.searchedText=""
+		this.state = { 
+			films: [],
+			isLoading: false
+		}
 	}
 
-_loadFilms() {
-    getFilmsFromApiWithSearchedText("star").then(data => {
-			this._films = data.results
-			this.forceUpdate()
-		})
+	_loadFilms() {
+		if(this.searchedText.length > 0) {
+			this.setState({ isLoading: true })
+			getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+				this.setState({ 
+					films: data.results,
+					isLoading: false
+				})
+			})
+		}
 	}
+
+_searchTextInputChanged(text) {
+	this.searchedText = text
+}
+
+_displayLoading() {
+	if (this.state.isLoading) {
+		return (
+			<View style={styles.loading_container}>
+				<ActivityIndicator size='large' />
+			</View>
+		)
+	}
+}
 
   render() {
     return (
       <View style={styles.main_container}>
-        <TextInput style={styles.textinput} placeholder='Titre du film'/>
+        <TextInput 
+					style={styles.textinput} 
+					placeholder='Titre du film'
+					onChangeText={(text) => this._searchTextInputChanged(text)}
+					onSubmitEditing={() => this._loadFilms()}
+					/>
         <Button title='Rechercher' onPress={() => this._loadFilms()}/>
         <FlatList
-          data={this._films}
+          data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({item}) => <FilmItem film={item}/>}
+					onEndReachThreshold={0.5}
+					onEndReached={() => {
+						console.log("onEndReached")
+					}}
         />
+				{this._displayLoading()}
       </View>
     )
   }
@@ -44,6 +77,15 @@ const styles = StyleSheet.create({
 		borderColor: '#000000',
 		borderWidth: 1,
 		paddingLeft: 5
+	},
+	loading_container: {
+		position: 'absolute',
+		left: 0,
+		right: 0,
+		top: 100,
+		bottom: 0,
+		alignItems: 'center',
+		justifyContent: 'center'
 	}
 })
 
