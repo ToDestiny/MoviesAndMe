654c043f9d23dad2da1c4854f0e6c55dd8cd4004
Anthony COLAS <anthonycolas.france@gmail.com>
2020-07-21 08:13:06 +0200

    Fix and clean code

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -2,8 +2,9 @@
 
 const API_TOKEN = "88cf3fe8ea6e42fbb4b0702eb46f5d38";
 
-export function getFilmsFromApiWithSearchedText (text) {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getFilmsFromApiWithSearchedText (text, page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + 
+'&language=fr&query=' + text + "&page=" + page
   return fetch(url)
     .then((response) => response.json())
     .catch((error) => console.error(error))
